2c7e83ce1d9afd9701b173eabb3fc45a41a4a248
h1m <himanshu.yadav643@gmail.com>
2021-01-12 21:38:02 +0530

    variables, operators, and functions

--- section_2/the_unconventional_calculator/assets/scripts/app.js
+++ section_2/the_unconventional_calculator/assets/scripts/app.js
@@ -1,1 +1,19 @@
-alert("This works!!!");
+/* eslint-disable */
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+let calculationDescription = "";
+
+// function declaration
+function add(num1, num2) {
+  const result = num1 + num2;
+  // alert(`The result is ${result}`);
+  return result;
+}
+
+currentResult = add(5, 5); // calling or invoking the function
+
+// currentResult = currentResult + 10;
+// calculationDescription = `(${defaultResult} + 10)`; // template literal
+
+outputResult(currentResult, "");
