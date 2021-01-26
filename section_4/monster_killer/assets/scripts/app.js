8cbaf9a31705b6457f26f1c2d1e7fc5c2b38deb5
h1m <himanshu.yadav643@gmail.com>
2021-01-26 07:42:52 +0530

    introduced strong attack

--- section_4/monster_killer/assets/scripts/app.js
+++ section_4/monster_killer/assets/scripts/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 15;
 
 let chosenMaxValue = 100;
 let currentMonsterHealth = chosenMaxValue;
@@ -9,8 +10,17 @@ let currentPlayerHealth = chosenMaxValue;
 adjustHealthBars(chosenMaxValue);
 
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
+  const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= monsterDamage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("You have a draw!");
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
