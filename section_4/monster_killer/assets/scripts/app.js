211239163c93c6201a5d7ef95e25e46dda9ac0a5
h1m <himanshu.yadav643@gmail.com>
2021-01-26 08:14:54 +0530

    refactored the code

--- section_4/monster_killer/assets/scripts/app.js
+++ section_4/monster_killer/assets/scripts/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 18;
 const MONSTER_ATTACK_VALUE = 15;
 
 let chosenMaxValue = 100;
@@ -9,8 +10,14 @@ let currentPlayerHealth = chosenMaxValue;
 
 adjustHealthBars(chosenMaxValue);
 
-function attackHandler() {
-  const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const monsterDamage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= monsterDamage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -23,4 +30,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
